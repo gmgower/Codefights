e21da41747d05e1e98dc5d4493851abbd61a7c49
mada <gmgower@gmail.com>
2017-11-15 13:38:11 -0800

    Third Commit

--- Intro/checkPalindrome.js
+++ Intro/checkPalindrome.js
@@ -1,10 +1,17 @@
 //Given the string, check if it is a palindrome. A palindrome is a string that reads the same left-to-right and right-to-left.
+//Solution 1
 function checkPalindrome(inputString){
-    for(var i = 0; i <= inputString.length/2; i++){
-        if(inputString[i] != inputString[(inputString.length - 1) - i]){
+    // Lowercase the string.
+   var str = inputString.toLowerCase();
+   // Create the FOR loop
+    for(var i = 0; i <= str.length/2; i++){
+   // As long as the characters from each part matech the FOR loop will continue
+        if(str[i] !== str[(str.length - 1) - i]){
+   //When the characters don't match, false is returned and exit the FOR loop
             return false;
         }
     }
+    //Bothh parts are strictly equal it return true => the string is paliindrome.
     return true;
 }
 
